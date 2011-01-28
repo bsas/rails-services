f290f26d04c9576bed11ea19bd4b5111ecbfdfd7
Bernardo Silva <bernardo.silva@gmail.com>
2011-01-28 12:33:59 -0600

    Adapting the code to work on Rails 2.3.5

--- lib/rails-services.rb
+++ lib/rails-services.rb
@@ -1,4 +1,13 @@
 module ActionController
+
+  # Rails 3 routes
+  # match '/info(.:format)' => 'application#info_services'
+  # match '/info/:resource(.:format)' => 'application#info_resource'
+
+  # Rails 2 routes
+  # map.connect 'info.:format', :controller => "application", :action => "info_services"
+  # map.connect 'info/:resource.:format', :controller => "application", :action => "info_resource"
+
   class Base
 
     def info_resource
@@ -17,46 +26,56 @@ module ActionController
 
     private
     def describe_resource(resource_name)
-      @map = {}
+      map    = {}
 
-      base = get_model(resource_name)
+      single = [:belongs_to, :has_one]
+      base   = get_model(resource_name)
       if base
         base.columns.each do |column|
-          @map[column.name] = {:type => column.type}
+          map[column.name] = {:type => column.type}
         end
 
         base.reflect_on_all_associations.each do |reflection|
-          @map[reflection.name] = {:type => get_type(reflection.class_name)}
-          @map[reflection.name][:collection] = true if reflection.collection?
+          map[reflection.name] = {:type => get_type(reflection.class_name)}
+          map[reflection.name][:collection] = true unless single.include? reflection.macro
+          map.delete(reflection.association_foreign_key)
         end
       else
-        @map = {:error => true}
+        map = {:error => true}
       end
 
-      {resource_name => @map}
+      # returns the resource graph
+      {resource_name => map}
     end
 
     def describe_services
-      list = []
+      list   = []
 
-      Rails.application.routes.routes.each do |route|
+      rails3 = Rails::VERSION::MAJOR > 2
+      routes = rails3 ? Rails.application.routes.routes.each : ActionController::Routing::Routes.routes
+
+      routes.each do |route|
+        verb         = ((rails3 ? route.verb : route.conditions[:method]) || :any).to_s.upcase
+        path         = rails3 ? route.path : route.segments.to_s.gsub(/\?$/, '')
+
         requirements = route.requirements
         index        = requirements[:action] == 'index'
         @type        = get_type(requirements[:controller])
 
         map          = {
-            :service => index ? requirements[:controller] : @type ?
+            :name => index ? requirements[:controller] : @type ?
                 "#{requirements[:action]}_#{@type}" :
                 "#{requirements[:action]}_#{requirements[:controller]}",
-            :verb    => route.verb,
-            :path    => route.path
+            :verb    => verb,
+            :path    => path
         }
         map[:type] = @type if @type
         map[:collection] = true if index
 
-        list << map unless route.verb.nil?
+        list << map unless verb.nil? or verb == 'ANY'
       end
 
+      # returns the services graph
       list
     end
 
